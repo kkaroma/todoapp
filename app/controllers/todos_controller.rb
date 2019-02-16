0f98be04ff226eb38b4dcc6361fc4452a0c4fba3
Kaizilege Karoma <kkaroma@gmail.com>
2019-02-16 16:30:31 +0300

    Added delete action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -37,6 +37,12 @@ def index
 	@todos = Todo.all
 end
 
+def destroy
+	@todo = Todo.find(params[:id])
+	@todo.destroy
+	flash[:notice] = "Todo was successfully destroyed"
+	redirect_to todos_path
+end
 
 
 private
